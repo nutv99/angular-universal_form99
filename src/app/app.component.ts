0b3ffec7f10f3f7a5d5df1b19e583bf829a09fce
nutv99 <nutv99@gmail.com>
2023-01-29 11:55:57 +0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
   staffForm: FormGroup; // <--- staffForm is of type FormGroup
@@ -15,15 +14,14 @@ export class AppComponent {
     // <--- inject FormBuilder
     this.createForm();
   }
-  formModel: any =
-   {
+  formModel: any = {
     field1: [Validators.required, Validators.maxLength(4)],
     field2: [],
     field3: [],
   };
 
   field1: any = 'Validators.required';
-  field1Type: string = 'number';
+  field1Type: string = 'file';
   field1Visible: boolean = true;
 
   createForm() {
@@ -31,7 +29,6 @@ export class AppComponent {
       name: ['', this.formModel.field1],
       name2: ['', this.formModel.field2],
       name3: ['', this.formModel.field3],
-     
     });
   }
 }
